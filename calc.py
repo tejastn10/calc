86edfbfd53babf226292cd0c5e2bb6dbd8de7fc1
tejastn10 <tejastn10@gmail.com>
2020-05-14 11:43:09 +0530

    Buttons function created

--- calc.py
+++ calc.py
@@ -46,6 +46,40 @@ class CalcUi(QMainWindow):
         # Add the display to the general layout
         self.generalLayout.addWidget(self.display)
 
+    def _createButtons(self):
+        """Create the buttons."""
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
+        # Button text | position on the QGridLayout
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4),
+                   }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+        # Add buttonsLayout to the general layout
+        self.generalLayout.addLayout(buttonsLayout)
+
 
 # Client code
 
